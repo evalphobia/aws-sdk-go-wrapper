5efab2c13073a826d490fe3bae9ff19460c891b0
evalphobia <evalphobia@gmail.com>
2018-03-01 17:04:32 +0900

    [rekognition] Add helper method for InvalidParameterException error (#27)

--- rekognition/error.go
+++ rekognition/error.go
@@ -2,10 +2,22 @@ package rekognition
 
 import "strings"
 
-var msgInvalidImageEncoding = `InvalidImageFormatException: Invalid image encoding`
+const (
+	msgInvalidImageEncoding = `InvalidImageFormatException: Invalid image encoding`
+	msgInvalidParameter     = `InvalidParameterException: Request has Invalid Parameters`
+)
 
 // IsErrorInvalidImageEncoding checks if gicen error is `Invalid image encoding`.
 func IsErrorInvalidImageEncoding(err error) (ok bool) {
+	if err == nil {
+		return false
+	}
+
+	return strings.Contains(err.Error(), msgInvalidImageEncoding)
+}
+
+// IsErrorInvalidParameter checks if gicen error is `Invalid image encoding`.
+func IsErrorInvalidParameter(err error) (ok bool) {
 	if err == nil {
 		return false
 	}
