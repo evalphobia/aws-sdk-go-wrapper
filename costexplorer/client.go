1c02827b5407508296e78194abd6d192dc4fb6fe
evalphobia <evalphobia@gmail.com>
2020-02-14 11:40:58 +0900

    [costexplorer] Add costexplorer.GetCostAndUsage

---
+++ costexplorer/client.go
@@ -0,0 +1,64 @@
+package costexplorer
+
+import (
+	SDK "github.com/aws/aws-sdk-go/service/costexplorer"
+
+	"github.com/evalphobia/aws-sdk-go-wrapper/config"
+	"github.com/evalphobia/aws-sdk-go-wrapper/log"
+)
+
+const (
+	serviceName = "CostExplorer"
+)
+
+// CostExplorer has *SDK.CostExplorer client.
+type CostExplorer struct {
+	client *SDK.CostExplorer
+
+	logger log.Logger
+}
+
+// New returns initialized *CostExplorer.
+func New(conf config.Config) (*CostExplorer, error) {
+	sess, err := conf.Session()
+	if err != nil {
+		return nil, err
+	}
+
+	svc := &CostExplorer{
+		client: SDK.New(sess),
+		logger: log.DefaultLogger,
+	}
+	return svc, nil
+}
+
+// SetLogger sets logger.
+func (svc *CostExplorer) SetLogger(logger log.Logger) {
+	svc.logger = logger
+}
+
+// GetCostAndUsage executes GetCostAndUsage operation with customized input.
+func (svc *CostExplorer) GetCostAndUsage(input GetCostAndUsageInput) (UsageResult, error) {
+	return svc.DoGetCostAndUsage(input.ToInput())
+}
+
+// DoGetCostAndUsage executes GetCostAndUsage operation.
+func (svc *CostExplorer) DoGetCostAndUsage(input *SDK.GetCostAndUsageInput) (UsageResult, error) {
+	output, err := svc.client.GetCostAndUsage(input)
+	if err != nil {
+		svc.Errorf("error on `GetCostAndUsage` operation; error=%w;", err)
+		return UsageResult{}, err
+	}
+
+	return NewUsageResult(output), nil
+}
+
+// Infof logging information.
+func (svc *CostExplorer) Infof(format string, v ...interface{}) {
+	svc.logger.Infof(serviceName, format, v...)
+}
+
+// Errorf logging error information.
+func (svc *CostExplorer) Errorf(format string, v ...interface{}) {
+	svc.logger.Errorf(serviceName, format, v...)
+}
