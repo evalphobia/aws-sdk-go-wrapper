d493e5fb8fbefa3f0ce36e696e877617c65a986a
evalphobia <evalphobia@gmail.com>
2020-03-13 14:14:55 +0900

    [cloudwatch] Add 'PutMetricData'

--- cloudwatch/request_type.go
+++ cloudwatch/request_type.go
@@ -44,8 +44,8 @@ func (o MetricStatisticsInput) ToInput() *SDK.GetMetricStatisticsInput {
 	if o.Unit != "" {
 		in.Unit = &o.Unit
 	}
-	in.Statistics = sliceStringToPointer(o.Statistics)
-	in.ExtendedStatistics = sliceStringToPointer(o.ExtendedStatistics)
+	in.Statistics = pointers.SliceString(o.Statistics)
+	in.ExtendedStatistics = pointers.SliceString(o.ExtendedStatistics)
 
 	in.Dimensions = make([]*SDK.Dimension, 0, len(o.DimensionsMap)+len(o.Dimensions))
 	for key, val := range o.DimensionsMap {
@@ -68,14 +68,108 @@ type Dimension struct {
 	Value string
 }
 
-func sliceStringToPointer(list []string) []*string {
-	if len(list) == 0 {
-		return nil
+type PutMetricDataInput struct {
+	MetricData []MetricDatum
+	Namespace  string
+}
+
+func (o *PutMetricDataInput) AddMetric(d MetricDatum) {
+	o.MetricData = append(o.MetricData, d)
+}
+
+func (o PutMetricDataInput) ToInput() *SDK.PutMetricDataInput {
+	in := &SDK.PutMetricDataInput{}
+	if o.Namespace != "" {
+		in.Namespace = &o.Namespace
 	}
 
-	result := make([]*string, len(list))
-	for i, v := range list {
-		result[i] = pointers.String(v)
+	in.MetricData = make([]*SDK.MetricDatum, 0, len(o.MetricData))
+	for _, v := range o.MetricData {
+		in.MetricData = append(in.MetricData, v.ToSDKValue())
 	}
-	return result
+	return in
+}
+
+type MetricDatum struct {
+	MetricName        string
+	Unit              string
+	StorageResolution int64
+	Value             float64
+	HasValue          bool // use as true when value == 0
+	Values            []float64
+	Counts            []float64
+	Timestamp         time.Time
+
+	StatisticValues StatisticSet
+	Dimensions      []Dimension
+}
+
+func (d MetricDatum) ToSDKValue() *SDK.MetricDatum {
+	in := &SDK.MetricDatum{}
+	if d.MetricName != "" {
+		in.MetricName = &d.MetricName
+	}
+	if d.Unit != "" {
+		in.Unit = &d.Unit
+	}
+	if d.StorageResolution != 0 {
+		in.StorageResolution = &d.StorageResolution
+	}
+	if d.HasValue || d.Value != 0 {
+		in.Value = &d.Value
+	}
+	in.Values = pointers.SliceFloat64(d.Values)
+	in.Counts = pointers.SliceFloat64(d.Counts)
+	if !d.Timestamp.IsZero() {
+		in.Timestamp = &d.Timestamp
+	}
+
+	in.StatisticValues = d.StatisticValues.ToSDKValue()
+	for _, v := range d.Dimensions {
+		in.Dimensions = append(in.Dimensions, &SDK.Dimension{
+			Name:  pointers.String(v.Name),
+			Value: pointers.String(v.Value),
+		})
+	}
+	return in
+}
+
+type StatisticSet struct {
+	Maximum     float64
+	Minimum     float64
+	SampleCount float64
+	Sum         float64
+
+	// use as true when value == 0
+	HasMaximum     bool
+	HasMinimum     bool
+	HasSampleCount bool
+	HasSum         bool
+}
+
+func (d StatisticSet) ToSDKValue() *SDK.StatisticSet {
+	hasValue := false
+
+	in := &SDK.StatisticSet{}
+	if d.HasMaximum || d.Maximum != 0 {
+		in.Maximum = &d.Maximum
+		hasValue = true
+	}
+	if d.HasMinimum || d.Minimum != 0 {
+		in.Minimum = &d.Minimum
+		hasValue = true
+	}
+	if d.HasSampleCount || d.SampleCount != 0 {
+		in.SampleCount = &d.SampleCount
+		hasValue = true
+	}
+	if d.HasSum || d.Sum != 0 {
+		in.Sum = &d.Sum
+		hasValue = true
+	}
+
+	if !hasValue {
+		return nil
+	}
+	return in
 }
